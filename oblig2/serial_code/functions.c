5a3cf772f2817bf6bcc772c316ac2b07f55bb09c
danaars <daarstein@gmail.com>
2022-05-02 15:56:23 +0200

    nice

---
+++ oblig2/serial_code/functions.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Image struct definition
+typedef struct image
+{   // Stores a 2D matrix of size m x n
+    float **image_data;
+    int m;
+    int n;
+} image;
+
+void allocate_image(image *u, int m, int n);
+void deallocate_image(image *u);
+void convert_jpeg_to_image(const unsigned char *image_chars, image *u);
+void convert_image_to_jpeg(const image *u, unsigned char *image_chars);
+void iso_diffusion_denoising(image *u, image *u_bar, float kappa, int iters);
+
+void allocate_image(image *u, int m, int n)
+{
+    u->m = m;
+    u->n = n;
+
+    u->image_data = (float**) malloc(m * sizeof(float**));
+    for (int i=0; i<m; i++){
+        u->image_data[i] = (float*) malloc(n * sizeof(float*));
+    }
+}   // End allocate_image
+
+void deallocate_image(image *u)
+{
+    for (int i=0; i<u->m; i++){
+        free(u->image_data[i]);
+    }
+    free(u->image_data);
+}   // End deallocate_image
+
+void convert_jpeg_to_image(const unsigned char *image_chars, image *u)
+{
+    //float max_float = (2 - pow(2, -23)) * pow(2, 127);
+    //float scaler = max_float / 255.0;
+    
+    //printf("input const char: %u\n", image_chars[0]);
+    //printf("Scaler: %f\n", scaler);
+    //printf("output float value: %f\n", image_chars[0]*scaler);
+    for (int i=0; i<u->m; i++){
+        for (int j=0; j<u->n; j++){
+             u->image_data[i][j] = (float) (image_chars[u->n * i + j]);// * scaler);
+        }
+    }    
+}   // End convert_jpeg_to_image
+
+void convert_image_to_jpeg(const image *u, unsigned char *image_chars)
+{
+    //float max_float = (2 - pow(2, -23)) * pow(2, 127);
+    //float scaler = 255.0/max_float;
+
+    //printf("input float value: %f\n", u->image_data[0][0]);
+    //printf("Scaler: %E\n", scaler);
+    //printf("output unsigned char: %u\n", (unsigned char) (u->image_data[0][0] * scaler));
+    for (int i=0; i<u->m; i++){
+        for (int j=0; j<u->n; j++){
+            image_chars[u->n*i + j] = (unsigned char) (u->image_data[i][j]);// * scaler);
+        }
+    }
+}   // End convert_image_to_jpeg
+
+void iso_diffusion_denoising(image *u, image *u_bar, float kappa, int iters)
+{
+    image *tmp;
+    for (int iter=0; iter<iters; iter++){
+        for (int i=1; i<u->m-1; i++){
+            for (int j=1; j<u->n-1; j++){
+                u_bar->image_data[i][j] = u->image_data[i][j] + kappa * 
+                    (u->image_data[i-1][j] + u->image_data[i][j-1] - 
+                     4*u->image_data[i][j] + u->image_data[i][j+1] + 
+                     u->image_data[i+1][j]);
+            }
+        }
+        // Swap pointer if not last iteration
+        if (iter < iters-1){
+            tmp = u_bar;
+            u_bar = u;
+            u = tmp; 
+        }
+    } 
+
+    // Doing the borders manually
+    // These are never included in the iterations loop
+    // and can thus be saved untill the end.
+    for (int i=0; i<u->m; i++){
+        u_bar->image_data[i][0] = u->image_data[i][0];
+        u_bar->image_data[i][u->n-1] = u->image_data[i][u->n-1];
+    }
+
+    for (int i=1; i<u->n-1; i++){
+        u_bar->image_data[0][i] = u->image_data[0][i];
+        u_bar->image_data[u->m-1][i] = u->image_data[u->m-1][i];
+    }
+}   // End iso_diffusion_denoising
---
+++ oblig2/serial_code/serial_main.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Image struct definition
+typedef struct image
+{   // Stores a 2D matrix of size m x n
+    float **image_data;
+    int m;
+    int n;
+} image;
+
+//#include "../one_folder/simple-jpeg/import_export_jpeg.h"
+// import_JPEG_file and export_JPEG_file defined in import_export_jpeg.c
+void import_JPEG_file(const char *filename, unsigned char **image_chars,
+        int *image_height, int *image_width, int *num_components);
+
+void export_JPEG_file(const char* filename, const unsigned char* image_chars,
+        int image_height, int image_width, int num_components, int quality);
+
+void allocate_image(image *u, int m, int n);
+void deallocate_image(image *u);
+void convert_jpeg_to_image(const unsigned char *image_chars, image *u);
+void convert_image_to_jpeg(const image *u, unsigned char *image_chars);
+void iso_diffusion_denoising(image *u, image *u_bar, float kappa, int iters);
+
+int main(int argc, char* argv[])
+{
+    int m, n, c, iters;
+    float kappa;
+    image u, u_bar;
+    unsigned char *image_chars;
+    char *input_jpeg_filename, *output_jpeg_filename;
+
+    // Read from command line
+    kappa = atof(argv[1]);
+    iters = atoi(argv[2]);
+    input_jpeg_filename = argv[3];
+    output_jpeg_filename = argv[4];
+    printf("Input JPEG: %s\n", input_jpeg_filename);
+
+    // m, n and c obtains values from this func
+    import_JPEG_file(input_jpeg_filename, &image_chars, &m, &n, &c);
+    printf("c = %d\n", c);
+    printf("Number of iterations: %d\n", iters);
+
+    allocate_image(&u, m, n);
+    allocate_image(&u_bar, m, n);
+    /*
+    printf("Before conversion, int top left pixel:%d\n", u.image_data[0][0]);
+    printf("Before conversion, unsigned char top left pixel:%u\n", u.image_data[0][0]);
+    printf("Before conversion, float top left pixel:%f\n", u.image_data[0][0]);
+    printf("Before conversion, char top left pixel:%c\n", u.image_data[0][0]);
+    */
+
+
+    convert_jpeg_to_image(image_chars, &u);
+    //printf("After conversion, unsigned char top left pixel:%u\n", u.image_data[0][0]);
+    //printf("After conversion, int top left pixel of u:%d\n", u.image_data[0][0]);
+    //printf("After conversion, float top left pixel of u:%f\n", u.image_data[0][0]);
+    //printf("Max float: %f\n", (2-pow(2, -23))*pow(2, 127));
+
+    iso_diffusion_denoising(&u, &u_bar, kappa, iters);
+
+    //printf("After denoising algorithm\n");
+    //printf("top left pixel u_bar: %f\n", u_bar.image_data[0][0]);
+    convert_image_to_jpeg(&u_bar, image_chars);
+    //convert_image_to_jpeg(&u_bar, image_chars);
+    export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
+
+    deallocate_image(&u);
+    deallocate_image(&u_bar);
+
+
+    return 0;
+}
